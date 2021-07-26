d08194df0e67e006cbb5b1d82d9ba18f94bafd5a
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-27 00:40:35 +0530

    Added:comparing whether two line are equal

--- Comparision.java
+++ Comparision.java
@@ -4,8 +4,8 @@ public class Comparision {
 	public static void main(String args[]){
 		System.out.println("Welcome to line comparision computation program");	
 		int x1,x2,y1,y2,a,b;
-		double len;
-		System.out.println("enter points of a line");
+		double len1,len2;
+		System.out.println("enter points of a line1");
 	    Scanner s = new Scanner(System.in);
 			x1=s.nextInt();
 			y1=s.nextInt();
@@ -13,8 +13,19 @@ public class Comparision {
 			y2=s.nextInt();
 			a=x2-x1;
 			b=y2-y1;
-		len=Math.sqrt((a*a)+(b*b));
-		System.out.println("length of line is" +len);
+		len1=Math.sqrt((a*a)+(b*b));
+		System.out.println("length of line1 is" +len1);
+		System.out.println("enter points of a line2");   
+		x1=s.nextInt();
+		y1=s.nextInt();
+		x2=s.nextInt();
+		y2=s.nextInt();
+		a=x2-x1;
+		b=y2-y1;
+		len2=Math.sqrt((a*a)+(b*b));
+		System.out.println("length of line2 is" +len2);
+	if (len1==len2)
+		System.out.println("both lines are equal");
 	
 	}
 }
