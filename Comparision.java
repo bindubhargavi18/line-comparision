3abdbe8b15732274fb8c44c8649447c9d92ff6b0
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-26 23:37:28 +0530

    Added:welcome message

---
+++ Comparision.java
@@ -0,0 +1,7 @@
+package lines;
+//import java.util.*;
+public class Comparision {
+	public static void main(String args[]){
+		System.out.println("Welcome to line comparision computation program");	
+	}
+}
