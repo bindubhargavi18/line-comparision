91b5e507233a7ec007805688f195c9e86fa2b596
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-27 00:37:47 +0530

    Added:calculated length of a line

--- Comparision.java
+++ Comparision.java
@@ -5,8 +5,8 @@ public class Comparision {
 		System.out.println("Welcome to line comparision computation program");	
 		int x1,x2,y1,y2,a,b;
 		double len;
-		System.out.println("enter satrting and ending points of a line");
-	    Scanner s = new Scanner(System.in);	   
+		System.out.println("enter points of a line");
+	    Scanner s = new Scanner(System.in);
 			x1=s.nextInt();
 			y1=s.nextInt();
 			x2=s.nextInt();
@@ -14,7 +14,7 @@ public class Comparision {
 			a=x2-x1;
 			b=y2-y1;
 		len=Math.sqrt((a*a)+(b*b));
-		System.out.println(len);
-			
+		System.out.println("length of line is" +len);
+	
 	}
 }
