e0db222564c611a7d3d21a3277b1c1f6c24dbcd0
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-27 19:23:27 +0530

    Added:equality of two lines

--- Comparision.java
+++ Comparision.java
@@ -1,7 +1,9 @@
 package lines;
 import java.util.*;
-public class Comparision {
-	public static void main(String args[]){
+public class Comparision
+{
+	public static void main(String args[])
+	{
 		System.out.println("Welcome to line comparision computation program");	
 		int x1,x2,y1,y2,a,b;
 		double len1,len2;
@@ -20,17 +22,18 @@ public class Comparision {
 		y1=s.nextInt();
 		x2=s.nextInt();
 		y2=s.nextInt();
+		s.close();
 		a=x2-x1;
 		b=y2-y1;
 		len2=(Math.sqrt((a*a)+(b*b)));
 		System.out.println("length of line2 is" +len2);
-		int res=Double.compare(len1,len2);
-	   if(res==0)
-		   System.out.println("Both lines are equal");
-	   else if(res<0)
-		   System.out.println("line1: " + len1 + " is less than line2: " + len2);
-	   else
-		   System.out.println("line1: " + len1 + " is greater than line2:  " + len2);
+		Double obj1=len1;
+		Double obj2=len2;
 	
+	   if(obj1.equals(obj2))
+		   System.out.println("Both lines are equal");
+	   else 
+		   System.out.println("Lines are not equal");
+	  
 	}
 }
